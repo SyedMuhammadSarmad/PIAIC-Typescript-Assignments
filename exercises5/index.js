db13205fdd20b5530976bfe18a86de2a01c9eff0
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 03:00:54 +0500

    added exercises5

--- exercises5/index.js
+++ exercises5/index.js
@@ -1,11 +1,8 @@
 #! /usr/bin/env node
-
 //Question 5
 //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-
-let quote:string = "Software is eating the world, but AI is going to eat software"
-let author:string = "Jensen Huang"
-
-let message:string = `${author} once said, "${quote}"`
-
-console.log(message)
+let quote = "Software is eating the world, but AI is going to eat software";
+let author = "Jensen Huang";
+let message = `${author} once said, "${quote}"`;
+console.log(message);
+export {};
