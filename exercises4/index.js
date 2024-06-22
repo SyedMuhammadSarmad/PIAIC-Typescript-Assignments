4bd69f97d901a4a3246ee363f2c3177c4e041ecb
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 02:45:26 +0500

    exercises4 added

--- exercises4/index.js
+++ exercises4/index.js
@@ -1,10 +1,8 @@
 #! /usr/bin/env  node
-
+"use strict";
 //Question 4
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, 
 //including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-
-let quote:string = "“Artificial intelligence will have a more profound impact on humanity than fire, electricity and the internet.”"
-let author:string = "Sundar Pichai"
-
-console.log(`${author} once said, ${quote}`)
+let quote = "“Artificial intelligence will have a more profound impact on humanity than fire, electricity and the internet.”";
+let author = "Sundar Pichai";
+console.log(`${author} once said, ${quote}`);
