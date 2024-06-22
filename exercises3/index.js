1e0e4c180d2a4219c17bab2e2aa58843cae43c36
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 02:19:35 +0500

    added exercises3

---
+++ exercises3/index.js
@@ -0,0 +1,5 @@
+let person_name = "sarmad";
+console.log("Lower case:", person_name.toLowerCase());
+console.log("Upper case:", person_name.toUpperCase());
+console.log("Tittle case:", person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
+export {};
