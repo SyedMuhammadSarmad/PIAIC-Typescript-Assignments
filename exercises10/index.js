dbe61853c6651a60ff9eaf99025a3b0ba56465b6
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 21:55:25 +0500

    added exercises10

--- exercises10/index.js
+++ exercises10/index.js
@@ -2,29 +2,23 @@
 //Question 10
 //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple 
 //at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-
-
 //exercise02
-let name:string = "Sarmad"
-
-console.log(`Hello ${name},how are you?`) 
-
+let name = "Sarmad";
+console.log(`Hello ${name},how are you?`);
 //use with back-ticks ``
-
 //exercise07
-console.log(4+4)
-console.log(16-8)
-console.log(4*2)
-console.log(16/2)
-
-/*comments have two 
+console.log(4 + 4);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(16 / 2);
+export {};
+/*comments have two
 types single line
 and multi line
 comments*/
-
 // -> single line
 /*
 
 MULTI LINES
 
-*/
+*/ 
