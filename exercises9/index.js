602aae97fe4ac04b0324cf67857b2d4e38b5fe0a
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 21:40:30 +0500

    exercises9 added

--- exercises9/index.js
+++ exercises9/index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+
 //Question 9
 //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 let favorite_number = 20;
