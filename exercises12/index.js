fbba28cf60a0ce7d788bc297e5f7c9f4509ac0e5
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 22:20:26 +0500

    added exercises12

--- exercises12/index.js
+++ exercises12/index.js
@@ -2,11 +2,10 @@
 //Question 12
 //Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text
 //of each message should be the same, but each message should be personalized with the person’s name.
-
-let names = ["Jawad","Abdullah","Wasiq","Umar"];
-
+let names = ["Jawad", "Abdullah", "Wasiq", "Umar"];
 let message = "Hi!";
-console.log(message+names[0])
-console.log(message+names[1])
-console.log(message+names[2])
-console.log(message+names[3])
+console.log(message + names[0]);
+console.log(message + names[1]);
+console.log(message + names[2]);
+console.log(message + names[3]);
+export {};
