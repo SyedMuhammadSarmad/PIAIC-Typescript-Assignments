acb6e9430e9d85240c9eb0d0fca63098001c7443
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 02:19:41 +0500

    added exercises21

---
+++ exercises21/index.ts
@@ -0,0 +1,16 @@
+//Question 21
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+type mytype = {
+    name: string,
+    age : number,
+    class : number,
+}
+
+let student : mytype ={
+    name : "sarmad",
+    age : 20,
+    class : 10
+}
+
+console.log(student)
