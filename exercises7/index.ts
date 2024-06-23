e888c1ae7680563192827ad742b5dd98cb1980ba
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 21:03:45 +0500

    added exercises7

--- exercises7/index.ts
+++ exercises7/index.ts
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+
 //Question 7
 //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 
