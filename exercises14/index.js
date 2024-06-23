c6ceab6592573e6b27ac844d7f4267c4e805b57d
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 22:54:32 +0500

    added exercises14

--- exercises14/index.js
+++ exercises14/index.js
@@ -2,7 +2,6 @@
 //Question 14
 //If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people
 //you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-
-let guest : string[] = ["Jawad","Abdullah","Aadil"];
-
-guest.map((guests)=>console.log(`Hi ${guests} I’d love for you to join us for dinner`))
+let guest = ["Jawad", "Abdullah", "Aadil"];
+guest.map((guests) => console.log(`Hi ${guests} I’d love for you to join us for dinner`));
+export {};
