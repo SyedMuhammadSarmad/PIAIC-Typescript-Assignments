95eeb2e2b1720a313a7f2e347b1b1dfb4c839ff2
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 03:28:48 +0500

    added exercises27

--- exercises27/index.js
+++ exercises27/index.js
@@ -7,45 +7,40 @@
 • If the alien is red, print a message that the player earned 15 points.
 
 • Write three versions of this program, making sure each message is printed for the appropriate color alien.*/
-
 //version1 color is green
 let alien_color = "green";
-console.log("allien color is ",alien_color)
-if(alien_color == "green"){
+console.log("allien color is ", alien_color);
+if (alien_color == "green") {
     console.log("the player earned 5 points");
 }
-else if(alien_color == "yellow"){
+else if (alien_color == "yellow") {
     console.log("the player earned 10 points");
 }
-else{
+else {
     console.log("the player earned 15 points");
 }
-
 //version2 color is yellow
-alien_color = "yellow"
-console.log("allien color is ",alien_color)
-if(alien_color == "green"){
+alien_color = "yellow";
+console.log("allien color is ", alien_color);
+if (alien_color == "green") {
     console.log("the player earned 5 points");
 }
-else if(alien_color == "yellow"){
+else if (alien_color == "yellow") {
     console.log("the player earned 10 points");
 }
-else{
+else {
     console.log("the player earned 15 points");
 }
-
 //version3 color is red
-alien_color = "red"
-console.log("allien color is ",alien_color)
-if(alien_color == "green"){
+alien_color = "red";
+console.log("allien color is ", alien_color);
+if (alien_color == "green") {
     console.log("the player earned 5 points");
 }
-else if(alien_color == "yellow"){
+else if (alien_color == "yellow") {
     console.log("the player earned 10 points");
 }
-else{
+else {
     console.log("the player earned 15 points");
 }
-
-
-
+export {};
