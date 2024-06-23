0507d131ca5c79f8323fc909d68983fd7785ce11
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 01:56:44 +0500

    added exercises19

--- exercises19/index.js
+++ exercises19/index.js
@@ -1,7 +1,6 @@
 //Question 19
 //Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-
-let guest : string[] = ["Jawad","Abdullah","Aadil"];
+let guest = ["Jawad", "Abdullah", "Aadil"];
 let n = guest.length;
-
-console.log(`I am inviting ${n} peoples to dinner`)
+console.log(`I am inviting ${n} peoples to dinner`);
+export {};
