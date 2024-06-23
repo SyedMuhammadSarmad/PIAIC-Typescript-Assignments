9038bf546abf3eff6fb58efbd85490b2935e2504
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 02:28:01 +0500

    added exercises22

---
+++ exercises22/index.ts
@@ -0,0 +1,12 @@
+//Question 22
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. 
+Make sure you correct the error before closing the program.*/
+
+
+let name = ["adnan","jawad","abdullah"]
+
+//index error
+console.log(name[4])
+
+//Make sure you correct the error before closing the program
+console.log([name[2]])
