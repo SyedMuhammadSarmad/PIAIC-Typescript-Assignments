1596a526516a723a0b9ab4f7fc45fda2a0b6a20c
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 22:11:00 +0500

    added exercises11

--- exercises11/index.js
+++ exercises11/index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 //Question 11
 //Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 let names = ["Jawad", "Abdullah", "Wasiq", "Umar"];
