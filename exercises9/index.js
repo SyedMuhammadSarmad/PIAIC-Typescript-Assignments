453a281e460a47658d64053cfcb4f6e720d7c33e
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-23 21:29:55 +0500

    exercises9 added

--- exercises9/index.js
+++ exercises9/index.js
@@ -2,4 +2,5 @@
 //Question 9
 //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 let favorite_number = 20;
-console.log(`my favorite number is ${favorite_number}`)
+console.log(`my favorite number is ${favorite_number}`);
+export {};
