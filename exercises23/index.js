24a7c6907ecd558f212da9e7842e7b45643011c8
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 02:39:42 +0500

    added exercises23

---
+++ exercises23/index.js
@@ -0,0 +1,27 @@
+//Question 23
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let age = 18;
+//5 tests evaluate to True
+console.log(18 == age);
+console.log(16 < age);
+console.log(age % 2 == 0);
+console.log(typeof (age) == 'number');
+console.log(typeof (age) != 'string');
+//5 tests evaluate to False
+console.log(18 != age);
+console.log(16 > age);
+console.log(age % 2 != 0);
+console.log(typeof (age) != 'number');
+console.log(typeof (age) == 'string');
+export {};
