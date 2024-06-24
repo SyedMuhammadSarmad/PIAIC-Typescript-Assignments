920050ee9305aab83768d963e40a2f2c07220808
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 13:56:19 +0500

    added exercises30

--- exercises30/index.js
+++ exercises30/index.js
@@ -4,12 +4,11 @@ to each user after they log in to a website. Loop through the array, and print a
 • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 
 • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.*/
-
-let user_name = ["Admin","Hamza","Eric","Jawad","Ali"]
-
-for( const user of user_name){
-    if(user == "admin")
-        console.log("Hello admin, would you like to see a status report?")
-    else 
-        console.log(`Hello ${user}, thank you for logging in again.`)
+let user_name = ["Admin", "Hamza", "Eric", "Jawad", "Ali"];
+for (const user of user_name) {
+    if (user == "admin")
+        console.log("Hello admin, would you like to see a status report?");
+    else
+        console.log(`Hello ${user}, thank you for logging in again.`);
 }
+export {};
