c0f60666ad84bc8f3cf3cd218742299e4a88a2ae
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-06-24 15:17:08 +0500

    added exercises36

---
+++ exercises36/index.js
@@ -0,0 +1,9 @@
+// Question 36
+/* T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.*/
+function make_shirt(size, text) {
+    console.log("Size of shirt is", size);
+    console.log("Messge should be print is", text);
+}
+make_shirt("medium", "Hello world");
+export {};
